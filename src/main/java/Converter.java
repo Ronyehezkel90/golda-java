6af35c8fd0d8cbc011e800fc13f419f113ee10fe
golanaor <36920962+golanaor@users.noreply.github.com>
2019-11-24 23:40:49 +0200

    naour push to organize

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -3,6 +3,26 @@ import okhttp3.OkHttpClient;
 public class Converter {
 
     void convert() {
-        OkHttpClient client = new OkHttpClient();
+        public static void main(String[] args) {
+
+            String url = "https://golda-go.herokuapp.com/get/branch" ;
+
+            OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .get()
+                    .build();
+            try {
+                Response response = client.newCall(request).execute();
+                System.out.println(response);
+                System.out.println(response.body());
+            } catch (Exception e) {
+                System.out.println("not working");
+                System.out.println(e.getMessage());
+            }
+
+        }
+
     }
 }
