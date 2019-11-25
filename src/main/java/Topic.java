2d9791129e2486119d47a43fe45e28bf1fa23793
golanaor <36920962+golanaor@users.noreply.github.com>
2019-11-25 22:57:16 +0200

    new CLASS

---
+++ src/main/java/Topic.java
@@ -0,0 +1,39 @@
+public class Topic {
+
+    private int id;
+    private  String name;
+    private  String comment;
+    private int general;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getGeneral() {
+        return general;
+    }
+
+    public void setGeneral(int general) {
+        this.general = general;
+    }
+}
