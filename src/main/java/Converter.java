77fd5c5628c393142ad4ba4003b940c29c7d0f32
golanaor <36920962+golanaor@users.noreply.github.com>
2019-11-25 23:01:37 +0200

    added classes and so

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -1,12 +1,20 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.List;
+
 
 public class Converter {
 
     public static void main(String[] args) {
 
-        String url = "https://golda-go.herokuapp.com/get/branch" ;
+        String url = "https://golda-go.herokuapp.com/get/branch";
 
         OkHttpClient client = new OkHttpClient();
 
@@ -17,11 +25,27 @@ public class Converter {
         try {
             Response response = client.newCall(request).execute();
             System.out.println(response);
-            System.out.println(response.body().string());
-        } catch (Exception e) {
+            String responseAsString = response.body().string();
+            System.out.println(responseAsString);
+
+            Gson gson = new Gson();
+            // Convert JSON File to Java Object
+            JSONObject jsonObject = new JSONObject(responseAsString);
+            System.out.println(responseAsString);
+
+//            Branch branch = gson.fromJson(responseAsString, );
+            // print staff
+//            System.out.println(branch);
+
+        } catch (IOException e) {
             System.out.println("not working");
             System.out.println(e.getMessage());
+            e.printStackTrace();
         }
+
+
+        System.out.println("check this:");
+
 
     }
 
