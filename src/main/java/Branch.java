687e31cd6888be1b43306434e4e2e02929cdc05f
golanaor <36920962+golanaor@users.noreply.github.com>
2019-11-25 22:58:50 +0200

    added charecthers and so

--- src/main/java/Branch.java
+++ src/main/java/Branch.java
@@ -1,5 +1,48 @@
 public class Branch {
 
+    private int ID;
+    private String Name;
+    private String Address;
+    private String Owner;
+    private Exam exams;
 
+    public int getID() {
+        return ID;
+    }
 
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getOwner() {
+        return Owner;
+    }
+
+    public void setOwner(String owner) {
+        Owner = owner;
+    }
+
+    public Exam getExams() {
+        return exams;
+    }
+
+    public void setExams(Exam exams) {
+        this.exams = exams;
+    }
 }
