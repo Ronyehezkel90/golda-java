a956fb5037ce630ee2af6f8240d6ada647485dcc
golanaor <36920962+golanaor@users.noreply.github.com>
2019-11-27 00:37:52 +0200

    succsed to save the json objects into Branch object with the properties: name,id,address,owner

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -56,9 +56,25 @@ public class Converter {
             String contents = new String(Files.readAllBytes(Paths.get(filePath)));
             JSONObject j = new JSONObject(contents);
             JSONArray branches = j.getJSONArray("branches");
+            Branch b[] = new Branch[branches.length()];
             for (int i = 0 ; i < branches.length(); i++){
-                System.out.println(branches.get(i));
+//                System.out.println(branches.get(i));
+                JSONObject object = branches.getJSONObject(i);
+//                System.out.println(object.getString("name"));
+//                System.out.println(object.getString("address"));
+                b[i] = new Branch();
+                b[i].setName(object.getString("name"));
+                b[i].setAddress(object.getString("address"));
+                b[i].setID(object.getInt("id"));
+                b[i].setOwner(object.getString("owner"));
+                System.out.println(b[i].getID());
+                System.out.println(b[i].getName());
+                System.out.println(b[i].getAddress());
+                System.out.println(b[i].getOwner());
+                System.out.println();
             }
+//            JSONObject j1 = j.getJSONObject("branches");
+//            System.out.println(j1.getString("name"));
 
         } catch (IOException e) {
             e.printStackTrace();
