c8c5eeb6039862e21eca3da931b9eb62395ce8c4
Ronyehezkel90 <ronyehezkel90@gmail.com>
2019-12-11 01:04:54 +0200

    Revert "With shauli"

---
+++ src/main/java/Converter.java
@@ -0,0 +1,28 @@
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class Converter {
+
+    public static void main(String[] args) {
+
+        String url = "https://golda-go.herokuapp.com/get/branch" ;
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            System.out.println(response);
+            System.out.println(response.body());
+        } catch (Exception e) {
+            System.out.println("not working");
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+}
